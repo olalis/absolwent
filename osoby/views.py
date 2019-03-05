2b3cca6e34b54ffc74d1358c45de311e3e44f294
olalis <aleksandralis2000@gmail.com>
2019-03-05 15:44:30 +0100

    widok test

--- osoby/views.py
+++ osoby/views.py
@@ -4,5 +4,8 @@ from django.http import HttpResponse
 def index(reqest):
     return HttpResponse("<h1>Witaj w django</h1")
 
+def test(reqest):
+    return HttpResponse("<h1>Coś tam Coś tam</h1")
+
 
 # Create your views here.
